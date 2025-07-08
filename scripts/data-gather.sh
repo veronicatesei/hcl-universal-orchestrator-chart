9996529dd362027dc57a285611807da64ee3c19c
veronicatesei <veronica.tesei@hcl.com>
2025-07-08 15:45:17 +0200

    datagather_scripts

--- scripts/data-gather.sh
+++ scripts/data-gather.sh
@@ -2,9 +2,33 @@
 
 NAMESPACE="$1"
 
+red()   { echo -e "\e[31m$*\e[0m"; }
+
 if [ -z "$NAMESPACE" ]; then
-  echo "Please provide the namespace as an argument."
+  red "Please provide the namespace as first argument."
   exit 1
+fi
+
+OCLI_PATH="$2"
+
+if [ -z "$OCLI_PATH" ]; then
+  red "Please provide the ocli path as second argument."
+  exit 1
+fi
+
+GATEWAY="$3"
+
+if [ -z "$GATEWAY" ]; then
+  echo "Gateway URL not provided. Attempting to fetch from Ingress."
+  REGEX="gateway\.[\d\.-]+\.([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}"
+  GATEWAY=$(kubectl get ingress -n "$NAMESPACE" | grep -o -E "$REGEX")
+  if [ -z "$GATEWAY" ]; then
+    red "No gateway URL found in Ingress. Please provide the gateway URL as third argument."
+    exit 1
+  fi
+  METRICS_URL="https://$GATEWAY/q/metrics"
+else
+  METRICS_URL="$GATEWAY/q/metrics"
 fi
 
 mkdir logs
@@ -29,20 +53,13 @@ do
     # Fetch the log file from the container and copy it to log folder
     kubectl cp "$NAMESPACE/$POD":opt/app/stdlist logs/"$CONTAINER_FOLDER"/ -c "$CONTAINER"
     # Fetch the metrics of the container
-    kubectl exec "$POD" -c "$CONTAINER" --namespace "$NAMESPACE" -- curl -sSk https://localhost:8443/q/metrics > logs/"$CONTAINER_FOLDER"/metrics.log
+    kubectl exec "$POD" -c "$CONTAINER" --namespace "$NAMESPACE" -- curl -sSk "$METRICS_URL" > logs/"$CONTAINER_FOLDER"/metrics.log
   done
 done
 #Zip the logs
 zip -r data-gather.zip logs
 #Delete the logs folder
 rm -rf logs
-
-OCLI_PATH="$2"
-
-if [ -z "$OCLI_PATH" ]; then
-  echo "Please provide the ocli path as an argument."
-  exit 1
-fi
 
 mkdir definitions
 #Extract all the Jobs
@@ -67,6 +84,13 @@ mkdir definitions
 "$OCLI_PATH" model extract definitions/allAPIKeys.txt from api=@
 #Extract all the event sources
 "$OCLI_PATH" model extract definitions/allEventSources.txt from eventsource=@
+#Extract all the resources
+"$OCLI_PATH" model extract definitions/allResources.txt from res=@/@
+#Extract all the human task queues
+"$OCLI_PATH" model extract definitions/allHumanTaskQueues.txt from htq=@/@
+#Extract all AI Agents
+"$OCLI_PATH" model extract definitions/allAIAgents.txt from aiagent=@/@
+
 
 #Zip the definitions
 zip -r data-gather.zip definitions
